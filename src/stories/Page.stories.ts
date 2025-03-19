9b3a02fc5a58774db4472b01b871c3c336e6362f
Stephen Wu <wujstephen@gmail.com>
2025-03-18 22:21:54 -0700

    Re-do storybook with ts/tsx

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,20 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react"
 import { expect, userEvent, within } from "@storybook/test"
 
 import { Page } from "./Page"
 
-export default {
+const meta = {
 	title: "Example/Page",
 	component: Page,
 	parameters: {
 		// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
 		layout: "fullscreen",
 	},
-}
+} satisfies Meta<typeof Page>
 
-export const LoggedOut = {}
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const LoggedOut: Story = {}
 
 // More on component testing: https://storybook.js.org/docs/writing-tests/component-testing
-export const LoggedIn = {
+export const LoggedIn: Story = {
 	play: async ({ canvasElement }) => {
 		const canvas = within(canvasElement)
 		const loginButton = canvas.getByRole("button", { name: /Log in/i })
