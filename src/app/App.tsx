9f5a23ddbeb0dd7bd4a995ce7a5981dc566945c7
Stephen Wu <wujstephen@gmail.com>
2025-03-19 00:41:25 -0700

    Dark mode on App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,42 +3,54 @@ import { useState } from "react"
 import "@/app/App.css"
 import reactLogo from "@/app/assets/react.svg"
 import viteLogo from "@/app/assets/vite.svg"
+import { ThemeProvider, useTheme } from "@/components/ThemeProvider"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 
 function App() {
 	const [count, setCount] = useState(0)
-
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<div className="flex items-center justify-center gap-4">
-				<a
-					href="https://vite.dev"
-					target="_blank"
-					className="hover:opacity-80 transition-opacity"
-				>
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a
-					href="https://react.dev"
-					target="_blank"
-					className="hover:opacity-80 transition-opacity"
-				>
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<Card className="w-[300px]">
-				<h1 className="text-2xl font-bold">Vite + React</h1>
-				<CardContent className="space-y-4">
-					<Button
-						onClick={() => setCount(count => count + 1)}
-						className="w-full"
+		<ThemeProvider>
+			<div className="flex flex-col items-center justify-center h-screen">
+				<div className="flex items-center justify-center gap-4">
+					<a
+						href="https://vite.dev"
+						target="_blank"
+						className="hover:opacity-80 transition-opacity"
 					>
-						count is {count}
-					</Button>
-				</CardContent>
-			</Card>
-		</div>
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+					<a
+						href="https://react.dev"
+						target="_blank"
+						className="hover:opacity-80 transition-opacity"
+					>
+						<img src={reactLogo} className="logo react" alt="React logo" />
+					</a>
+				</div>
+				<Card className="w-[300px]">
+					<h1 className="text-2xl font-bold">Vite + React</h1>
+					<CardContent className="space-y-4">
+						<Button
+							className="w-full"
+							onClick={() => setCount(count => count + 1)}
+						>
+							count is {count}
+						</Button>
+						<ThemeToggle />
+					</CardContent>
+				</Card>
+			</div>
+		</ThemeProvider>
+	)
+}
+
+function ThemeToggle() {
+	const { theme, toggleTheme } = useTheme()
+	return (
+		<Button className="w-full" onClick={() => toggleTheme()}>
+			Toggle Theme: {theme}
+		</Button>
 	)
 }
 
---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,107 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react"
+
+type Theme = "dark" | "light" | "system"
+
+type ThemeProviderProps = {
+	children: React.ReactNode
+	defaultTheme?: Theme
+	storageKey?: string
+}
+
+type ThemeProviderState = {
+	theme: Theme
+	setTheme: (theme: Theme, persist?: boolean) => void
+	toggleTheme: (persist?: boolean) => void
+}
+
+const initialState: ThemeProviderState = {
+	theme: "system",
+	setTheme: () => null,
+	toggleTheme: () => null,
+}
+
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+
+export function ThemeProvider({
+	children,
+	defaultTheme = "system",
+	storageKey = "vite-ui-theme",
+	...props
+}: ThemeProviderProps) {
+	const [theme, setTheme] = useState<Theme>(
+		() => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
+	)
+	const root = window.document.documentElement
+
+	useEffect(() => {
+		// First mount -> set theme based on system preference
+		const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
+		const matches = mediaQuery.matches
+		const persistedTheme =
+			(localStorage.getItem(storageKey) as Theme) ?? "system"
+		if (persistedTheme === "dark" || persistedTheme === "light") {
+			setTheme(persistedTheme)
+			root.classList.remove("light", "dark")
+			root.classList.add(persistedTheme)
+		} else {
+			setTheme(matches ? "dark" : "light")
+			root.classList.remove("light", "dark")
+			root.classList.add(matches ? "dark" : "light")
+		}
+
+		// Subsequent system theme updates -> update theme if not explicitly set
+		const updateTheme = (e: MediaQueryListEvent) => {
+			const persistedTheme =
+				(localStorage.getItem(storageKey) as Theme) ?? "system"
+			if (persistedTheme !== "light" && persistedTheme !== "dark") {
+				const newTheme = e.matches ? "dark" : "light"
+				root.classList.remove("light", "dark")
+				root.classList.add(newTheme)
+				setTheme(newTheme)
+			}
+		}
+		mediaQuery.addEventListener("change", updateTheme)
+		return () => mediaQuery.removeEventListener("change", updateTheme)
+	}, [root, storageKey])
+
+	const value = useMemo(
+		() => ({
+			theme,
+			setTheme: (theme: Theme, persist?: boolean) => {
+				console.log("setTheme", theme, persist)
+				if (persist) {
+					localStorage.setItem(storageKey, theme)
+				}
+				setTheme(theme)
+				root.classList.remove("light", "dark")
+				root.classList.add(theme)
+			},
+			toggleTheme: (persist?: boolean) => {
+				console.log("toggleTheme", persist)
+				if (persist) {
+					localStorage.setItem(storageKey, theme === "dark" ? "light" : "dark")
+				}
+				const newTheme = theme === "dark" ? "light" : "dark"
+				setTheme(newTheme)
+				root.classList.remove("light", "dark")
+				root.classList.add(newTheme)
+			},
+		}),
+		[theme, root, storageKey],
+	)
+
+	return (
+		<ThemeProviderContext.Provider {...props} value={value}>
+			{children}
+		</ThemeProviderContext.Provider>
+	)
+}
+
+export const useTheme = () => {
+	const context = useContext(ThemeProviderContext)
+
+	if (context === undefined)
+		throw new Error("useTheme must be used within a ThemeProvider")
+
+	return context
+}
