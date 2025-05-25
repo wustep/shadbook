5f3ba11f41c0412254f73e7b1db904bec0372a07
Stephen Wu <wujstephen@gmail.com>
2025-05-25 11:56:40 -0700

    Fix cards

--- src/app/pages/cards.tsx
+++ src/app/pages/cards.tsx
@@ -2,7 +2,7 @@ import { CardsDemo } from "@/storybook/app/components/cards"
 
 export function CardsPage() {
 	return (
-		<div className="py-4 px-6">
+		<div className="@container/main py-4 px-6">
 			<CardsDemo />
 		</div>
 	)
