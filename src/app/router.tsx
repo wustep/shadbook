c032fa72f6d6d8c997060ab9c20d1b3cbdef4b41
Stephen Wu <wujstephen@gmail.com>
2025-05-25 01:10:27 -0700

    Preload

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,7 +4,10 @@ import { createRouter, RouterProvider } from "@tanstack/react-router"
 // Import the generated route tree
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({
+	routeTree,
+	defaultPreload: "intent",
+})
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
